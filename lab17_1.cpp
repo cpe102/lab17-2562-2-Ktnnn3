ba9e578d7c39aa3dd271c7ad64b7abb4bcd00d5f
Ktnnn3 <59277393+Ktnnn3@users.noreply.github.com>
2020-03-09 23:27:31 +0700

    Update lab17_1.cpp

--- lab17_1.cpp
+++ lab17_1.cpp
@@ -1,91 +1,50 @@
-//Write your code here
-#include <iostream>
-#include <cstring> 
-#include <string>
-#include <fstream>
-#include <cctype>
-#include <vector>
+#include<iostream>
+#include<fstream>
+#include<string>
+#include<vector>
 using namespace std;
-
 int main(){
-
-    ifstream fin("name_score.txt");
-    string s,rule,fix,fix2;
-    vector<string> a;
-    vector<string> grade;
-    char name[100],point[100];
-    int na,nb,nc,nd = 0;
-    int count=0,check =0,check2 =0;
-    while(getline(fin,s)){
-        a.push_back(s);
-        count++;
-
-    }
-    for(int i=0;i<count;i++){
-        const char *c = a[i].c_str();
-        char format[] = "%[^:]: %d %d %d";
-        sscanf(c,format,name,&na,&nb,&nc);
-        nd = na+nb+nc;
-        a.at(i)=name;
-        grade.push_back(nd >= 80 ? "A": nd >= 70 ? "B" : nd >= 60 ? "C" : nd >= 50 ? "D" :"F");
-
-    }
-    string command;
-
-    for(;;){
-
-        cout << "---------------------------------\n";
-        cout << "Please input your command: ";
-        getline(cin,command);
-         if(rule == "exit")
-            break;
-        cout << "---------------------------------\n";
-        rule = command.substr(0,5);
-        if(rule == "name "|| "NAME" ){
-            command = command.erase(0,5);
-            for(int i=0;i<count;i++){
-                fix = a[i];
-                for(int j=0;j<fix.size();j++){
-                    fix[j]=toupper(fix[j]); 
-                }
-                for(int k=0;k<command.size();k++){
-                    command[k]=toupper(command[k]); 
-                }
-                if(command == fix){
-                    cout << a[i] << "'s grade = " << grade[i] << "\n" ;
-                    check2++;
-                }
-            }
-            if(check2 ==0){
-                cout <<"Cannot found.\n";
-            }
-
-
-        }else if(rule == "grade"){
-            command = command.erase(0,6);
-            
-            for(int i=0;i<count;i++){
-                for(int k=0;k<command.size();k++){
-                    command[k]=toupper(command[k]); 
-                }
-
-                if(command == grade[i]){
-                    cout << a[i] << "\n";
-                    check++;
-                }
-            }
-            if(check ==0){
-                cout <<"Cannot found.\n";
-            }
-
-        }else if(rule == "exit"){
-            break;
-        }else{
-            cout << "Invalid command\n";
-        }
-
-    }
-
-
+	ifstream n_s("name_score.txt");
+	string n,cm;
+	int n1,n2,n3;
+	vector<int>sum;
+	char name[100];
+	vector<string> vn,vg;
+	while(getline(n_s,n)){
+		sscanf(n.c_str(),"%[^:]: %d %d %d",&n1,&n2,&n3);
+		vn.push_back(name);
+		sum.push_back(n1+n2+n3);
+	}
+	for(int i=0;i<sum.size();i++){
+		if(sum[i]>=80) vg.push_back("A");
+		else if(sum[i]>=70) vg.push_back("B");
+		else if(sum[i]>=60) vg.push_back("C");
+		else if(sum[i]>=50) vg.push_back("D");
+		else vg.push_back("F");
+	}
+	
+	while(true){
+		cout<<"Please input your command: ";
+		getline(cin,cm);
+		int idx=cm.find_first_of(" ");
+		string st2=upper(cm.substr(idx+1,cm.size()));//option
+		string st1=upper(cm.substr(0,idx));//order
+		if(st1=="EXIT") break;
+		else if(st1=="NAME"){
+			for(int i=0;i<20;i++){
+				cout<<endl;
+			}
+				for(int j=0;j<vn.size();j++){
+					if(st2==upper(vn[i])) cout<<vn[i]<<"'s grade"<<vg[i]<<endl;
+					else if(st2 != upper(vn[i])&& i != vn.size()-1);
+					else cout<<"Cannot found";	
+				}
+			cout<<endl;
+				for(int p=0;p<20;p++) cout<<"-";
+				cout<<endl;
+		}
+		else cout<<"Invalid command"<<endl;
+	}
+	
+	
 }
-
