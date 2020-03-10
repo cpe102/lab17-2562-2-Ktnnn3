222197c2e2813d676f8d9b861761feebc95157ae
lab314-15@labeng.local <lab314-15@labeng.local>
2020-03-10 17:29:17 +0700

    lab17

--- lab17_1.cpp
+++ lab17_1.cpp
@@ -1,50 +1,75 @@
+//Write your code here
 #include<iostream>
-#include<fstream>
 #include<string>
+#include<cstring>
+#include<fstream>
 #include<vector>
+#include<cctype>
 using namespace std;
+
+string upper(string s){
+    for(int i=0;i<s.size();i++){
+           s[i] =  toupper(s[i]) ;
+        }
+        return s;
+}
+
+
 int main(){
-	ifstream n_s("name_score.txt");
-	string n,cm;
-	int n1,n2,n3;
-	vector<int>sum;
-	char name[100];
-	vector<string> vn,vg;
-	while(getline(n_s,n)){
-		sscanf(n.c_str(),"%[^:]: %d %d %d",&n1,&n2,&n3);
-		vn.push_back(name);
-		sum.push_back(n1+n2+n3);
+    ifstream source;
+    source.open("name_score.txt");
+    string textline;
+    vector<string> v_name,v_grade;
+    char name[100];
+    int a,b,c;
+    
+    while( getline(source,textline)){ //read each line of file
+    	
+    	sscanf(textline.c_str(),"%[^:]: %d %d %d",name,&a,&b,&c);
+        v_name.push_back(name);
+        int total = a+b+c;
+        v_grade.push_back(total >= 80 ? "A" : total>=70 ? "B" : total >= 60 ? "C" : total >= 50 ? "D" : "F");
+        cout<<name<<" "<<a<<" "<<b<<" "<<c<<" = "<<total<<endl;
 	}
-	for(int i=0;i<sum.size();i++){
-		if(sum[i]>=80) vg.push_back("A");
-		else if(sum[i]>=70) vg.push_back("B");
-		else if(sum[i]>=60) vg.push_back("C");
-		else if(sum[i]>=50) vg.push_back("D");
-		else vg.push_back("F");
-	}
-	
-	while(true){
-		cout<<"Please input your command: ";
-		getline(cin,cm);
-		int idx=cm.find_first_of(" ");
-		string st2=upper(cm.substr(idx+1,cm.size()));//option
-		string st1=upper(cm.substr(0,idx));//order
-		if(st1=="EXIT") break;
-		else if(st1=="NAME"){
-			for(int i=0;i<20;i++){
-				cout<<endl;
-			}
-				for(int j=0;j<vn.size();j++){
-					if(st2==upper(vn[i])) cout<<vn[i]<<"'s grade"<<vg[i]<<endl;
-					else if(st2 != upper(vn[i])&& i != vn.size()-1);
-					else cout<<"Cannot found";	
-				}
-			cout<<endl;
-				for(int p=0;p<20;p++) cout<<"-";
-				cout<<endl;
-		}
-		else cout<<"Invalid command"<<endl;
-	}
-	
-	
+    string type,search;
+  	while(1) {
+          
+  		cout<<"Please input your command : ";
+        getline(cin,textline);
+        int idx= textline.find_first_of(" ");
+        int i=0;
+       
+      
+        string s1=upper(textline.substr(0,idx));
+        string s2=upper(textline.substr(idx+1,textline.size()));
+        
+        //string s2 = upper(textline,substr(..........)); //option ; a,b,c,...,sanji,...
+        // if(s1=="NAME") {
+            
+        // }
+        if (s1 == "GRADE") {
+            for(int i=0;i<v_name.size();i++) {
+                if (s2==v_grade[i]) cout << v_name[i]<<endl;
+            }
+            cout<<"-----------------------------------"<<endl;
+        }
+        else if (s1=="NAME") {
+            for(int i=0;i<v_name.size();i++){
+                string ss = v_name[i].substr(0,v_name[i].size());
+                if (s2==upper(v_name[i]))  {
+                    cout <<v_name[i]<<"'s grade = "<<v_grade[i]<<endl; 
+                    break;
+                    }
+                else if (s2!=upper(v_name[i]) && i != v_name.size()-1);
+                else cout<<"Cannot found."<<endl;
+            }
+            cout<<"-----------------------------------"<<endl;
+        }
+        else if (s1 == "EXIT") break;
+        else cout<<"Invalind command."<<endl<<"-----------------------------------"<<endl;
+   
+  		
+  		//type exit ; break
+      
+}
 }
